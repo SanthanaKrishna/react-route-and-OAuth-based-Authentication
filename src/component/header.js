1f8a95eb20df18c96ce07de068a49336baa7801b
Santhana Krishna <36031304+SanthanaKrishna@users.noreply.github.com>
2019-03-20 00:55:26 +0530

    navigate link rule

--- src/component/header.js
+++ src/component/header.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className="ui secondary pointing menu">
+        {/* //bad Approach,Bcoz Dom in create broweser for every hit link using herf tag
+             <a href="/pageTwo">Navigate to page two</a> */}
                 <Link to="/" className="item">
                     Stream
                 </Link>
