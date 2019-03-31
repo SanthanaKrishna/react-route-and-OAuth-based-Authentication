37797fef50521cca9e120b39822bdf6cae386d60
Santhana Krishna <36031304+SanthanaKrishna@users.noreply.github.com>
2019-03-31 22:52:35 +0530

    redux dev tool

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import { createStore, Middleware, compose, applyMiddleware } from 'redux'
 import { App } from './App';
 import reducers from './reducers';
+// control action state using query string below
+// http://localhost:3000/?debug_session=logged_in  =>redux state stay even refearch page seen in redux dev tool
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers)
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware())
+);
+
 ReactDom.render(
     <Provider store={store}>
         <App />
     </Provider>
     , document.querySelector('#root')
-); 
+);
